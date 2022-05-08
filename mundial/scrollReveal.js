1940d6282c0f3f5a9b7663601238e640dd0c28db
Julian <juliaximo@gmail.com>
2022-05-08 08:55:27 -0300

    Adding js to generate fade in effect

---
+++ mundial/scrollReveal.js
@@ -0,0 +1,17 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("activee");
+    } else {
+      reveals[i].classList.remove("activee");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
