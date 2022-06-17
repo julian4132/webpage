d556eef247be1c32f53fb4bbda50fad45e4e9494
Ignacio Ortego <ignacioortego110@gmail.com>
2022-06-17 20:27:59 -0300

    hover animations on icons and social-media links

--- mundial/index.php
+++ mundial/index.php
@@ -144,8 +144,11 @@ if(isset($_SESSION['user_name'])){
             <h1 class="reveal fade-right-slow"><b>Quienes Somos</b></h1>
             <hr>
             <p class="reveal fade-right">
-            Somos una organización sin fines de lucro que apunta a ser una fuente de información de excelencia acerca de la Copa Mundia de Fútbol Qatar 2022. Este proyecto, inicialmente promovido por cuatro estudiantes del Instituto Politécnico Superior, aspira a fomentar las actividades deportivas, lúdicas y sociales, consiguiendo generar un espacio propicio para cultivar y transmitir la pasión por este deporte emblemático, que nos hace tan argentinos.
-Nosotros creemos que, si hay algo que nos define, es el compromiso. El compromiso por garantizar el acceso a información actualizada y verídica, obtenida a partir de fuentes certificadas y presentada de manera simple, clara y concisa.
+            Somos una organización sin fines de lucro que apunta a ser una fuente de información de excelencia acerca de la Copa Mundia de Fútbol Qatar 2022. 
+            Este proyecto, inicialmente promovido por cuatro estudiantes del Instituto Politécnico Superior, aspira a fomentar las actividades deportivas, lúdicas y sociales, 
+            consiguiendo generar un espacio propicio para cultivar y transmitir la pasión por este deporte emblemático, que nos hace tan argentinos.
+            Nosotros creemos que, si hay algo que nos define, es el compromiso. El compromiso por garantizar el acceso a información actualizada y verídica, 
+            obtenida a partir de fuentes certificadas y presentada de manera simple, clara y concisa.
             </p> 
         </div>
         <div>
